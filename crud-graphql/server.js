1cb54dbb69b67d4c1e0a351fed4e0709d40c9904
Jimena <jimena@Mac-mini-de-Jimena.local>
2023-12-15 12:04:34 -0300

    implement server web

---
+++ crud-graphql/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', function(req, res){
+    res.send('Bienvenido')
+})
+
+app.listen(8000, function(){
+    console.log("Servidor iniciado")
+})
+
