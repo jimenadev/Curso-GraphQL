0f6f9e61ff88c5969b1c7daff6f8b9e1fdb7c7b4
Jimena <jimena@Mac-mini-de-Jimena.local>
2023-12-17 16:36:43 -0300

    generating JSON Web Token

--- db/models/user.js
+++ db/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const secret = require('../libs/env').secret
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -37,6 +39,9 @@ userSchema.statics.authenticate = async function({email, password}){
 
     if(!result) throw new Error("Email or password are wrong")
 
+    //JSON Web Token
+    user.token = jwt.sign({id: user.id}, secret)
+    await user.save()
     return user
 }
 
